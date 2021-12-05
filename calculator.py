f9e1615101564a1d7cdad756039256051fe2f44c
frankennstein <karastoqnov.alexandar@gmail.com>
2021-12-05 23:58:38 +0200

    Last Update: 05.12.2021

--- calculator.py
+++ calculator.py
@@ -1,27 +1,15 @@
-calc_operation = input("Enter the operation: \n [+] for addition \n [-] for substract \n [*] for multiply \n [/] for division \n")
-a = float(input())
-b = float(input())
-result = 0
-
-while True:
-    
-    if calc_operation == "+":
-        result = a + b
-        print(f"{result:.2f}")
-    if calc_operation == "-":
-        result = a - b
-        print(f"{result:.2f}")
-    if calc_operation == "*":
-        result = a * b
-        print(f"{result:.2f}")       
-    if calc_operation == "/":
-        result = a / b
-        print(f"{result:.2f}")
-        
-        
-    a = float(input())
-    b = float(input())
-    if a == "stop" or b == 'stop':
-        break
-
-print(f"{result:.2f}")
+class Calculator:
+     
+     
+     def additon():
+         pass
+     
+     def substraction():
+         pass
+     
+     def multiply():
+         pass
+     
+     def divide():
+         pass
+     
