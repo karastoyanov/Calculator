a6212c2d41bfcc3f7be95b1cf5d8bd1ddbd6eef7
frankennstein <karastoqnov.alexandar@gmail.com>
2021-12-06 00:16:12 +0200

    Last Update: 06.12.2021

--- calculator.py
+++ calculator.py
@@ -1,15 +1,37 @@
+from tkinter import *
+from PIL import ImageTk
+
 class Calculator:
+     root = Tk()
+     root.title("Calculator")
+     root.geometry("200x300")
+     
+     image_num_zero = 'F:\Python_Projects\Personal Projects\Calculator\images\zero.png'
+     image_num_one = 'F:\Python_Projects\Personal Projects\Calculator\images\number-1.png'
+     image_num_two = 'F:\Python_Projects\Personal Projects\Calculator\images\two.png'
+     image_num_three = 'F:\Python_Projects\Personal Projects\Calculator\images\three.png'
+     image_num_four = 'F:\Python_Projects\Personal Projects\Calculator\images\four.png'
+     image_num_five = 'F:\Python_Projects\Personal Projects\Calculator\images\five.png'
+     image_num_six = 'F:\Python_Projects\Personal Projects\Calculator\images\six.png'
+     image_num_seven = 'F:\Python_Projects\Personal Projects\Calculator\images\seven.png'
+     image_num_eight = 'F:\Python_Projects\Personal Projects\Calculator\images\eight.png'
+     image_num_nine = 'F:\Python_Projects\Personal Projects\Calculator\images\nine.png'
      
      
      def additon():
          pass
      
+     
      def substraction():
          pass
+     
      
      def multiply():
          pass
      
+     
      def divide():
          pass
-     
+
+
+mainloop()
